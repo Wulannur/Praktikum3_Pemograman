f5856ce6151788deba5450c46b84a585f189a8cb
Wulan Nur Annisah <wulannurannisah470@gmail.com>
2021-12-02 21:27:30 +0700

    Add files via upload

---
+++ P4_4_1204007/P4_4_1204007/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace P4_4_1204007
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tbUpload = new System.Windows.Forms.Button();
+            this.tbExit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.tbNama = new System.Windows.Forms.TextBox();
+            this.tbNPM = new System.Windows.Forms.TextBox();
+            this.tbNoHP = new System.Windows.Forms.TextBox();
+            this.tbEmail = new System.Windows.Forms.TextBox();
+            this.tbKapital = new System.Windows.Forms.TextBox();
+            this.tbKecil = new System.Windows.Forms.TextBox();
+            this.tbTinggi = new System.Windows.Forms.TextBox();
+            this.tbBerat = new System.Windows.Forms.TextBox();
+            this.epWarning = new System.Windows.Forms.ErrorProvider(this.components);
+            this.epWrong = new System.Windows.Forms.ErrorProvider(this.components);
+            this.epCorrect = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.epWarning)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.epWrong)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.epCorrect)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbUpload
+            // 
+            this.tbUpload.Location = new System.Drawing.Point(326, 259);
+            this.tbUpload.Name = "tbUpload";
+            this.tbUpload.Size = new System.Drawing.Size(75, 23);
+            this.tbUpload.TabIndex = 16;
+            this.tbUpload.Text = "Upload";
+            this.tbUpload.UseVisualStyleBackColor = true;
+            // 
+            // tbExit
+            // 
+            this.tbExit.Location = new System.Drawing.Point(498, 259);
+            this.tbExit.Name = "tbExit";
+            this.tbExit.Size = new System.Drawing.Size(75, 23);
+            this.tbExit.TabIndex = 17;
+            this.tbExit.Text = "Exit";
+            this.tbExit.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "NAMA";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(49, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "NPM";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(49, 136);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "NO HP";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(49, 178);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "EMAIL";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(495, 55);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(181, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "NAMA (DENGAN HURUF KAPITAL)";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(495, 100);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(167, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "NAMA (DENGAN HURUF KECIL)";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(495, 136);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(84, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "TINGGI BADAN";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(495, 178);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(83, 13);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "BERAT BADAN";
+            // 
+            // tbNama
+            // 
+            this.tbNama.Location = new System.Drawing.Point(164, 55);
+            this.tbNama.Name = "tbNama";
+            this.tbNama.Size = new System.Drawing.Size(237, 20);
+            this.tbNama.TabIndex = 10;
+            // 
+            // tbNPM
+            // 
+            this.tbNPM.Location = new System.Drawing.Point(164, 97);
+            this.tbNPM.Name = "tbNPM";
+            this.tbNPM.Size = new System.Drawing.Size(237, 20);
+            this.tbNPM.TabIndex = 11;
+            // 
+            // tbNoHP
+            // 
+            this.tbNoHP.Location = new System.Drawing.Point(164, 136);
+            this.tbNoHP.Name = "tbNoHP";
+            this.tbNoHP.Size = new System.Drawing.Size(237, 20);
+            this.tbNoHP.TabIndex = 12;
+            // 
+            // tbEmail
+            // 
+            this.tbEmail.Location = new System.Drawing.Point(164, 171);
+            this.tbEmail.Name = "tbEmail";
+            this.tbEmail.Size = new System.Drawing.Size(237, 20);
+            this.tbEmail.TabIndex = 13;
+            // 
+            // tbKapital
+            // 
+            this.tbKapital.Location = new System.Drawing.Point(699, 47);
+            this.tbKapital.Name = "tbKapital";
+            this.tbKapital.Size = new System.Drawing.Size(222, 20);
+            this.tbKapital.TabIndex = 14;
+            // 
+            // tbKecil
+            // 
+            this.tbKecil.Location = new System.Drawing.Point(699, 92);
+            this.tbKecil.Name = "tbKecil";
+            this.tbKecil.Size = new System.Drawing.Size(222, 20);
+            this.tbKecil.TabIndex = 15;
+            // 
+            // tbTinggi
+            // 
+            this.tbTinggi.Location = new System.Drawing.Point(699, 128);
+            this.tbTinggi.Name = "tbTinggi";
+            this.tbTinggi.Size = new System.Drawing.Size(222, 20);
+            this.tbTinggi.TabIndex = 16;
+            // 
+            // tbBerat
+            // 
+            this.tbBerat.Location = new System.Drawing.Point(699, 170);
+            this.tbBerat.Name = "tbBerat";
+            this.tbBerat.Size = new System.Drawing.Size(222, 20);
+            this.tbBerat.TabIndex = 17;
+            // 
+            // epWarning
+            // 
+            this.epWarning.ContainerControl = this;
+            this.epWarning.Icon = ((System.Drawing.Icon)(resources.GetObject("epWarning.Icon")));
+            // 
+            // epWrong
+            // 
+            this.epWrong.ContainerControl = this;
+            this.epWrong.Icon = ((System.Drawing.Icon)(resources.GetObject("epWrong.Icon")));
+            // 
+            // epCorrect
+            // 
+            this.epCorrect.ContainerControl = this;
+            this.epCorrect.Icon = ((System.Drawing.Icon)(resources.GetObject("epCorrect.Icon")));
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(933, 334);
+            this.Controls.Add(this.tbBerat);
+            this.Controls.Add(this.tbTinggi);
+            this.Controls.Add(this.tbKecil);
+            this.Controls.Add(this.tbKapital);
+            this.Controls.Add(this.tbEmail);
+            this.Controls.Add(this.tbNoHP);
+            this.Controls.Add(this.tbNPM);
+            this.Controls.Add(this.tbNama);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbExit);
+            this.Controls.Add(this.tbUpload);
+            this.Name = "Form1";
+            this.Text = "BIODATA DIRI";
+            ((System.ComponentModel.ISupportInitialize)(this.epWarning)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.epWrong)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.epCorrect)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button tbUpload;
+        private System.Windows.Forms.Button tbExit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox tbNama;
+        private System.Windows.Forms.TextBox tbNPM;
+        private System.Windows.Forms.TextBox tbNoHP;
+        private System.Windows.Forms.TextBox tbEmail;
+        private System.Windows.Forms.TextBox tbKapital;
+        private System.Windows.Forms.TextBox tbKecil;
+        private System.Windows.Forms.TextBox tbTinggi;
+        private System.Windows.Forms.TextBox tbBerat;
+        private System.Windows.Forms.ErrorProvider epWarning;
+        private System.Windows.Forms.ErrorProvider epWrong;
+        private System.Windows.Forms.ErrorProvider epCorrect;
+    }
+}
+
